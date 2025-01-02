47451e743acfc597cb33092c20f7b0e6dec20af3
ziqo-maker <quizziqo@gmail.com>
2025-01-02 01:06:00 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Inter} from 'next/font/google'
+import Script  from "next/script";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({subsets:['latin']})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}
       >
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive"/>
         {children}
       </body>
     </html>
