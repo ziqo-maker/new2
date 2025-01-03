9d7adeda75282a4f61a60ae715159b7310d54d96
ziqo-maker <quizziqo@gmail.com>
2025-01-03 10:08:30 -0800

    Update route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({ error: 'Invalid user data' }, { status: 400 })
         }
 
-           let user = await prisma.user.findUnique({
+        let user = await prisma.user.findUnique({
             where: { telegramid: userData.id }
         })
 
