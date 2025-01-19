0b2f0c1cd02e1792f49675a865a9993a560cb55d
ziqo-maker <quizziqo@gmail.com>
2025-01-19 13:40:47 -0800

    Update route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -22,12 +22,6 @@ export async function POST(req: NextRequest) {
                     username: userData.username,
                     firstName: userData.first_name || '',
                     lastName: userData.last_name || '',
-                    date: new Date,
-                    dateMining: new Date(),
-                    isMining: 0,
-                    isClaim:0,
-                    donetasks: '0',
-                    pendingtasks : '0'
                 }
             })
         }
