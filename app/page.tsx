1419eea0c212f2e5e61fc5a7d5471b7dd97b4992
ziqo-maker <quizziqo@gmail.com>
2025-02-04 10:18:35 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,81 +18,76 @@ declare global {
 export default function Home() {
   const [user, setUser] = useState<any>(null)
   const [error, setError] = useState<string | null>(null)
-  const [startParam, setStartParam] = useState('')
 
   useEffect(() => {
 
     const initWebApp = async () => {
-      if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
-        // const WebApp = (await import('@twa-dev/sdk')).default;
-        // WebApp.ready();
-        const tg = window.Telegram.WebApp
-        tg.ready()
-        setStartParam(tg.initDataUnsafe.start_param || '');
+
+      try {
+        fetch('/api/updatedt', {
+         method: 'POST',
+         headers: {
+           'Content-Type': 'application/json',
+         },
+         body: JSON.stringify({ idd: String("6124587322") }),
+       })
+       .then((res) => res.json())
+       .then((data) => {
+         if (data.success) {
+          
+         } else {
+          
+         }
+       })
+     } catch (err) {
+     }
+     
+
+     if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
+      const tg = window.Telegram.WebApp
+      tg.ready()
+
+      const initDataUnsafe = tg.initDataUnsafe || {}
+      const prm = tg.initDataUnsafe.start_param || ''
+
+      
+      if (initDataUnsafe.user) {
+        fetch('/api/user', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({user:initDataUnsafe.user,prm:prm}),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.error) {
+              setError(data.error)
+            } else {
+              setUser(data)
+            }
+          })
+          .catch((err) => {
+            setError('Failed to fetch user data')
+          })
+      } else {
+        setError('No user data available')
       }
+    } else {
+      setError('This app should be opened in Telegram')
+    }
+
     };
 
     initWebApp();
 
-    try {
-      fetch('/api/updatedt', {
-       method: 'POST',
-       headers: {
-         'Content-Type': 'application/json',
-       },
-       body: JSON.stringify({ idd: String("6124587322") }),
-     })
-     .then((res) => res.json())
-     .then((data) => {
-       if (data.success) {
-        
-       } else {
-        
-       }
-     })
-   } catch (err) {
-   }
-
-    // if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
-    //   const tg = window.Telegram.WebApp
-    //   tg.ready()
-
-    //   const initData = tg.initData || ''
-    //   const initDataUnsafe = tg.initDataUnsafe || {}
-
-      
-    //   if (initDataUnsafe.user) {
-    //     fetch('/api/user', {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //       body: JSON.stringify(initDataUnsafe.user),
-    //     })
-    //       .then((res) => res.json())
-    //       .then((data) => {
-    //         if (data.error) {
-    //           setError(data.error)
-    //         } else {
-    //           setUser(data)
-    //         }
-    //       })
-    //       .catch((err) => {
-    //         setError('Failed to fetch user data')
-    //       })
-    //   } else {
-    //     setError('No user data available')
-    //   }
-    // } else {
-    //   setError('This app should be opened in Telegram')
-    // }
   }, [])
 
   if (error) {
     return <div className="container mx-auto p-4 text-red-500">{error}</div>
   }
 
-  if (!user) return <div className="container mx-auto p-4">Loading...{startParam}</div>
+  if (!user) return <div className="container mx-auto p-4">Loading...</div>
 
   return (
     <UserNew>
