557e40e076c6507e7a89062354fc8d1be04e21e6
ziqo-maker <quizziqo@gmail.com>
2025-02-05 08:54:14 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,126 +1,127 @@
-'use client'
+  'use client'
 
-import { useEffect, useState } from 'react'
-import { WebApp } from '@twa-dev/types'
-import { TabProvider } from '@/contexts/TabContext'
-import NavigationBar from "@/components/NavigationBar"
-import TabContainer from "@/components/TabContainer"
-import { UserNew } from '@/contexts/UserContextB'
+  import { useEffect, useState } from 'react'
+  import { WebApp } from '@twa-dev/types'
+  import { TabProvider } from '@/contexts/TabContext'
+  import NavigationBar from "@/components/NavigationBar"
+  import TabContainer from "@/components/TabContainer"
+  import { UserNew } from '@/contexts/UserContextB'
 
-declare global {
-  interface Window {
-    Telegram?: {
-      WebApp: WebApp
+  declare global {
+    interface Window {
+      Telegram?: {
+        WebApp: WebApp
+      }
     }
   }
-}
 
-export default function Home() {
-  const [user, setUser] = useState<any>(null)
-  const [error, setError] = useState<string | null>(null)
-  const [prm, setPrm] = useState<string>('')
+  export default function Home() {
+    const [user, setUser] = useState<any>(null)
+    const [error, setError] = useState<string | null>(null)
+    const [prm, setPrm] = useState<string>('')
 
-  useEffect(() => {
+    useEffect(() => {
 
-    const initWebApp = async () => {
+      const initWebApp = async () => {
 
-      try {
-        fetch('/api/updatedt', {
-         method: 'POST',
-         headers: {
-           'Content-Type': 'application/json',
-         },
-         body: JSON.stringify({ idd: String("6124587322") }),
-       })
-       .then((res) => res.json())
-       .then((data) => {
-         if (data.success) {
-          
-         } else {
-          
-         }
-       })
-     } catch (err) {
-     }
-     
-
-     if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
-      const tg = window.Telegram.WebApp
-      tg.ready()
-
-      const initDataUnsafe = tg.initDataUnsafe || {}
-
-      setPrm(tg.initDataUnsafe.start_param || '')
-
-      if (initDataUnsafe.user) {
-        fetch('/api/user', {
+        try {
+          fetch('/api/updatedt', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify(initDataUnsafe.user),
+          body: JSON.stringify({ idd: String("6124587322") }),
         })
-          .then((res) => res.json())
-          .then((data) => {
-            if (data.error) {
-              setError(data.error)
-            } else {
-              setUser(data)
-              if(prm.length != 0){
-                try {
-                  fetch('/api/invitereferal', {
-                   method: 'POST',
-                   headers: {
-                     'Content-Type': 'application/json',
-                   },
-                   body: JSON.stringify({ prm:prm,idd: String("6435568801") }),
-                 })
-                 .then((res) => res.json())
-                 .then((data) => {
-                   if (data.success) {
-                    
-                   } else {
-                    
-                   }
-                 })
-               } catch (err) {
-               }
-              }
-            }
-          })
-          .catch((err) => {
-            setError('Failed to fetch user data')
-          })
-      } else {
-        setError('No user data available')
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.success) {
+            
+          } else {
+            
+          }
+        })
+      } catch (err) {
       }
-    } else {
-      setError('This app should be opened in Telegram')
+      
+
+      if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
+        const tg = window.Telegram.WebApp
+        tg.ready()
+
+        const initDataUnsafe = tg.initDataUnsafe || {}
+
+        setPrm(tg.initDataUnsafe.start_param || '')
+
+        if (initDataUnsafe.user) {
+          fetch('/api/user', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(initDataUnsafe.user),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              if (data.error) {
+                setError(data.error)
+              } else {
+                setUser(data)
+                if(prm.length != 0){
+setError("s")
+                  try {
+                    fetch('/api/invitereferal', {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ prm:prm,idd: String("6435568801") }),
+                  })
+                  .then((res) => res.json())
+                  .then((data) => {
+                    if (data.success) {
+                      
+                    } else {
+                      
+                    }
+                  })
+                } catch (err) {
+                }
+                }
+              }
+            })
+            .catch((err) => {
+              setError('Failed to fetch user data')
+            })
+        } else {
+          setError('No user data available')
+        }
+      } else {
+        setError('This app should be opened in Telegram')
+      }
+
+      };
+
+      initWebApp();
+
+    }, [])
+
+    if (error) {
+      return <div className="container mx-auto p-4 text-red-500">{error}</div>
     }
 
-    };
+    if (!user) return <div className="container mx-auto p-4">Loading...{error}</div>
 
-    initWebApp();
-
-  }, [])
-
-  if (error) {
-    return <div className="container mx-auto p-4 text-red-500">{error}</div>
-  }
-
-  if (!user) return <div className="container mx-auto p-4">Loading...</div>
-
-  return (
-    <UserNew>
-  
-<TabProvider>
-      <main className='main-h-screen bg-white '>
-        <TabContainer/>
-        <NavigationBar/>
-      </main>
-    </TabProvider>
- 
-    </UserNew>
+    return (
+      <UserNew>
     
-  )
-}
+  <TabProvider>
+        <main className='main-h-screen bg-white '>
+          <TabContainer/>
+          <NavigationBar/>
+        </main>
+      </TabProvider>
+  
+      </UserNew>
+      
+    )
+  }
