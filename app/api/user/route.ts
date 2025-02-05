8f413abdb5f967d1942de18ab28015bafd929836
ziqo-maker <quizziqo@gmail.com>
2025-02-05 09:16:19 -0800

    Update route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,65 +1,40 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
-import { useContext } from 'react';
 
 export async function POST(req: NextRequest) {
     try {
-        const userData = await req.json()
         
-      
-        if (!userData || !userData.id) {
-            return NextResponse.json({ error: 'Invalid user data' }, { status: 400 })
-        }
+        const {prm,idd} = await req.json()
 
-       let user = await prisma.user.findFirst({
-            where: { idd: String(userData.id) }
-        })
-
-        // const gtprm = prm || ''
-
-        // if(String(gtprm).length != 0){
-        //     let userB = await prisma.user.findFirst({
-        //         where: { idd: String(gtprm) }
-        //     })
-        //     const str: string = String(userB?.invite)+','+String(userData.id);
-        //     await prisma.user.update({
-        //         where: { idd:String(gtprm) },
-        //         data: {  
-        //             invite : str
-        //         }
-        //     })
-            
-        // }
-        
-        
+          
 
       
-        if (!user) {
-            user = await prisma.user.create({
-                data: {
-                    idd: String(userData.id),
-                    username: userData.username,
-                    firstName: userData.first_name || '',
-                    lastName: userData.last_name || '',
-                    donetasks: '',
-                    pendingtasks : '',
-                    tokenvalue: '0.00000001',
-                    upgrade :'1,',
-                    donecreatedtasks :'',
-                    invite:'',
-                    pendingcreatedtasks:'',
-                    referal:''
+            let userB = await prisma.user.findFirst({
+                where: { idd: prm }
+            })
+            if(userB){
+
+                const str: string = String(userB?.invite)+','+String(idd);
+            await prisma.user.update({
+                where: { idd:prm },
+                data: {  
+                    invite : str
                 }
             })
-        }else{
 
-        }
+            await prisma.user.update({
+                where: { idd },
+                data: {  
+                    referal : prm
+                }
+            })
 
-
-
-        return NextResponse.json(user)
+            }
+            
+        
+        return NextResponse.json({id:idd})
     } catch (error) {
         console.error('Error processing user data:', error)
-        return NextResponse.json({ error: error }, { status: 500 })
+        return NextResponse.json({ error: 'server error' }, { status: 500 })
     }
 }
