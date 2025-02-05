91e47e73c624b48ec2da8b7517e1def6372119e3
ziqo-maker <quizziqo@gmail.com>
2025-02-05 08:38:06 -0800

    Update route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -57,7 +57,7 @@ export async function POST(req: NextRequest) {
 
 
 
-        return NextResponse.json('')
+        return NextResponse.json(user)
     } catch (error) {
         console.error('Error processing user data:', error)
         return NextResponse.json({ error: error }, { status: 500 })
