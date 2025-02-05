0c95a4e1818982d9bac0ea9d1c425a19c238a5e4
ziqo-maker <quizziqo@gmail.com>
2025-02-05 08:42:41 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -66,26 +66,26 @@ export default function Home() {
               setError(data.error)
             } else {
               setUser(data)
-              // if(prm.length != 0){
-              //   try {
-              //     fetch('/api/invitereferal', {
-              //      method: 'POST',
-              //      headers: {
-              //        'Content-Type': 'application/json',
-              //      },
-              //      body: JSON.stringify({ prm:prm,idd: String("6435568801") }),
-              //    })
-              //    .then((res) => res.json())
-              //    .then((data) => {
-              //      if (data.success) {
+              if(prm.length != 0){
+                try {
+                  fetch('/api/invitereferal', {
+                   method: 'POST',
+                   headers: {
+                     'Content-Type': 'application/json',
+                   },
+                   body: JSON.stringify({ prm:prm,idd: String("6435568801") }),
+                 })
+                 .then((res) => res.json())
+                 .then((data) => {
+                   if (data.success) {
                     
-              //      } else {
+                   } else {
                     
-              //      }
-              //    })
-              //  } catch (err) {
-              //  }
-              // }
+                   }
+                 })
+               } catch (err) {
+               }
+              }
             }
           })
           .catch((err) => {
