8db88efd599f64359779296b99055054fac6972a
ziqo-maker <quizziqo@gmail.com>
2025-02-13 09:03:44 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -119,7 +119,7 @@
 
                <Image 
         src={StartPic as StaticImageData} 
-      className="size-36 animate-waving-hand aspect-square object-fit"
+      className="size-20 animate-waving-hand aspect-square object-fit"
       alt=""
     />
            
