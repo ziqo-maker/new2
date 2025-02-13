12d02195c10b00ec2ce7e8ae56da5939e9e313de
ziqo-maker <quizziqo@gmail.com>
2025-02-13 11:47:19 -0800

    Update route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,14 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
-import { NewUserContext } from '@/contexts/UserContextB';
-import React from 'react';
 
 
 export async function POST(req: NextRequest) {
     try {
         const userData = await req.json()
-
-         // const { UserDt,setUserData } = React.useContext(NewUserContext);
         
         if (!userData || !userData.id) {
             return NextResponse.json({ error: 'Invalid user data' }, { status: 400 })
@@ -38,16 +34,10 @@ export async function POST(req: NextRequest) {
                     selectcharacter:1
                 }
             })
-            // setUserData({idd:String(userData.id),gtpoint:"0",selectcharacter:"1",speedlvl:"1",
-            //     upgrade:"1",username:String(userData.username),value:"0.00000001"
-            //   })
-        }else{
-        //   setUserData({idd:String(userData.id),gtpoint:String(user.points),selectcharacter:String(user.selectcharacter),speedlvl:String(user.speedlvl),
-        //     upgrade:String(user.upgrade),username:String(user.username),value:String(user.tokenvalue)
-        //   })
         }
 
-        return NextResponse.json(user)
+        return NextResponse.json({idd:String(userData.id),gtpoint:String(user.points),selectcharacter:String(user.selectcharacter),speedlvl:String(user.speedlvl),
+            upgrade:String(user.upgrade),username:String(user.username),value:String(user.tokenvalue)})
     } catch (error) {
         console.error('Error processing user data:', error)
         return NextResponse.json({ error: error }, { status: 500 })
