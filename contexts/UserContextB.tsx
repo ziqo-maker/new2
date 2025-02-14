d42514efb02191347e3a4f49b5b351d390b211c0
ziqo-maker <quizziqo@gmail.com>
2025-02-14 01:04:50 -0800

    Update UserContextB.tsx

--- contexts/UserContextB.tsx
+++ contexts/UserContextB.tsx
@@ -26,66 +26,9 @@ export const NewUserContext = React.createContext<ContextProps>({
   });
 
 export function UserNew({ children }: { children: React.ReactNode }) {
-    const [UserDt, setUserData] = React.useState<UserDt | null>({idd:'',gtpoint:'0',speedlvl:'1',upgrade:'1',selectcharacter:'1',value:'0.00000001',username:''});
+    const [UserDt, setUserData] = React.useState<UserDt | null>({idd:'0',gtpoint:'0',speedlvl:'1',upgrade:'1',selectcharacter:'1',value:'0.00000001',username:''});
 
-  const loadUserData = async () => {
-     if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
-            const tg = window.Telegram.WebApp
-            tg.ready()
-    
-            const initDataUnsafe = tg.initDataUnsafe || {}
-            const prm = tg.initDataUnsafe.start_param|| ''
-           
-            if (initDataUnsafe.user) {
-              fetch('/api/user', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(initDataUnsafe.user),
-              })
-                .then((res) => res.json())
-                .then((data) => {
-                  if (data.error) {
-                 
-                  } else {
-                    setUserData({idd:String(data.idd),gtpoint:String(data.points),selectcharacter:String(data.selectcharacter),speedlvl:String(data.speedlvl),
-                      upgrade:String(data.upgrade),username:String(data.username),value:String(data.tokenvalue)
-                    })
-    
-                    if(prm.length > 0){
-                      try {
-                        fetch('/api/invitereferal', {
-                        method: 'POST',
-                        headers: {
-                          'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({ idd:String(prm),idb: String(data.idd),referal:String(prm) }),
-                      })
-                      .then((res) => res.json())
-                      .then((data) => {
-                        if (data.success) {
-                          
-                        } else {
-                          
-                        }
-                      })
-                    } catch (err) {
-                    }
-                    }
-                    
-                    
-                  }
-                })
-                .catch((err) => {
-                })
-            } else {
-              
-            }
-          } else {
-           
-          } 
-  };
+  const loadUserData = async () => { };
 
   const value = {
     UserDt,
