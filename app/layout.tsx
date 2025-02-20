e57fce1d9a429f1b1c95b135274077a3217ea39f
ziqo-maker <quizziqo@gmail.com>
2025-02-20 08:54:37 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,34 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        import type { Metadata } from "next"
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Script from 'next/script'
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Telegram Mini App',
+  description: 'A simple Telegram mini app using Next.js and Prisma',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>    
+      <Script async data-mbid-banner="296125" src="https://js.mbidadm.com/banner/mbid.bnr.m.js" data-mbid-banner-ar="_gn"  strategy="beforeInteractive"/>
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
+        {children}
         
+      </body>
+    </html>
+  )
+}
+
         <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
         {children}
 
