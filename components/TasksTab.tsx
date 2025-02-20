6a603d6942fe22f73d21fd2eff81f964d5cc7fbd
ziqo-maker <quizziqo@gmail.com>
2025-02-20 01:44:49 -0800

    Update TasksTab.tsx

--- components/TasksTab.tsx
+++ components/TasksTab.tsx
@@ -45,8 +45,15 @@ type TaskCreated = {
   text:string
 }
 
+type Visit = {
+  id:string
+  click?:false,
+}
+
 const TasksTab = () => {
 
+     const [gtVisit,setVisit] = useState<Visit[]>([]);
+   
     const [gtTasks,setTask] = useState<Task[]>([]);
     const [pendingtasks,setPendingTasks] = useState<string> ();
     const [gtdonetasks,setDoneTasks] = useState<string> ();
@@ -60,7 +67,56 @@ const TasksTab = () => {
     const [refresh, setRefresh] = useState<boolean>(false);
    const timerRef = useRef<NodeJS.Timeout | null>(null);
       const [refreshB, setRefreshB] = useState<boolean>(false);
+      const [hideVisit, setHideVisit] = useState<boolean>(false);
 
+
+      const handleVisit = async(click:boolean | undefined) => {
+        if(!click){
+          var newData = gtVisit.map(el => {
+            if(el.id == "1")
+               return Object.assign({}, el, {click:true})
+            return el
+        });
+        setVisit(newData)
+        window.open("https://www.effectiveratecpm.com/uesqg4qxs?key=9bd32098d78c65aa13ea9d74ee52eea2"); 
+       setTimeout(() => {
+          
+        try {
+          fetch('/api/claim', {
+           method: 'POST',
+           headers: {
+             'Content-Type': 'application/json',
+           },
+           body: JSON.stringify({ idd: String(UserDt?.idd),miningPoint: 2000 }),
+         })
+         .then((res) => res.json())
+         .then((data) => {
+           if (data.success) {
+            const plus = Number(UserDt?.gtpoint) + 2000
+            setUserData({idd:String(UserDt?.idd),speedlvl:String(UserDt?.speedlvl),gtpoint:String(plus),selectcharacter:String(UserDt?.selectcharacter),upgrade:String(UserDt?.upgrade),value:String(UserDt?.value),username:String(UserDt?.username)})
+            const lcl = 2000
+            new Toast({
+              position: "top-center",
+              toastMsg: `You're received ${lcl} WalkCoin`,
+              autoCloseTime: 4500,
+              canClose: true,
+              showProgress: true,
+              pauseOnHover: true,
+              pauseOnFocusLoss: true,
+              type: "default",
+              theme: "light"
+            });
+            
+          }
+          setHideVisit(true)
+         })
+       } catch (err) {
+       }
+    
+      }, 9000);
+
+      }
+     }
     const handlePendingB = async(id:string,pendingcreatedtasks:string | undefined,click:boolean | undefined,isDoing:boolean | undefined,url:string) => {
      
       if(!click && !isDoing){
@@ -390,6 +446,14 @@ const TasksTab = () => {
     };
 
     },[refreshB])
+
+    useEffect(() => {
+      let model = {
+        id:"1",
+     }
+     gtVisit.push(model)
+    },[])
+  
     
     return (
         <div className=" flex justify-center overflow-auto">
@@ -439,6 +503,57 @@ const TasksTab = () => {
         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
     </svg>
                 </div>
+                {gtVisit.map((task,index) => {
+                  if(Loading == false && hideVisit == false){
+                    return(
+                      <center key={index}>
+                              <div className="h-3"/>
+                               <div className="w-[calc(100%-2rem)] flex-1 mt-1 px-3  items-center bg-[#ffae19]/[0.9] border-white border-4 border-double rounded-full py-[5px] ">
+                               <div className="grow flex items-center">
+                               <Image
+          src={Etc}
+        className="w-11 h-11  aspect-square object-cover  "
+        alt="Shiba Inu"
+      />
+                             
+                <div className="px-2"/>
+                <div className="grow space-y-1">
+                <p className="text-white font-Large text-lg text-wrap text-left">Visit a website and earn</p>
+                <div className="grow flex items-center space-x-1">
+                              <Image
+          src={FootPrint as StaticImageData}
+        className="w-7 h-7 "
+        alt=""
+      />     
+                <p className="text-white font-normal  text-base">+{Number(2000).toLocaleString()}</p> 
+                              </div>
+                </div>
+                <div className="px-1"/>
+                <div className="flex items-center">
+               
+                <div className=" px-1"/>
+                <button onClick={() => {handleVisit(task.click)}} className={`${task.click? 'glowwhite bg-white' : 'bg-black' } flex w-16 h-8 text-center items-center justify-center rounded-full px-3 py-[3px]`}>
+                <p className={`text-white font-Large ${task.click? 'text-[0px]' : ''}`}>Start</p>
+                  <div className="flex">
+                  <svg aria-hidden="true" className={`${task.click? 'w-6 h-6' : 'w-0 h-0'} flex text-gray-200 animate-spin dark:text-gray-600 fill-white`} viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+      </svg>
+                  </div>
+     
+                </button> 
+                
+                </div>
+  
+                               </div>                          
+                              
+                  
+                              </div>
+                            </center>
+                    )
+                  }
+                  
+                  }) }
                 {gtTasksCreated.map((task,index) => {
                   if(task.havekeyword){
 
