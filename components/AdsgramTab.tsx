4ffd9b694ffe22bf203c5c5b1f759ef57afc2918
ziqo-maker <quizziqo@gmail.com>
2025-02-27 02:09:50 -0800

    Add files via upload

--- components/AdsgramTab.tsx
+++ components/AdsgramTab.tsx
@@ -1,80 +1,26 @@
 'use client'
 
-import Image, {StaticImageData} from "next/image"
-import FootPrint from '@/icons/footprint.svg';
-import { useEffect,useState } from "react"
-import React from 'react';
+import { useCallback, ReactElement } from 'react'
+import { useAdsgram } from "./useAdsgram";
 import { ShowPromiseResult } from "@/types/adsgram";
 
 const Adsgram = () => {
 
-  const [refresh, setRefresh] = useState<boolean>(false);
- 
-     useEffect(() => {
-          
-        const AdController = window.Adsgram?.init({
-            blockId: "int-8537",
-            debug: true,
-            debugBannerType: "FullscreenMedia"
-          });
-             
-        //  const showPromise: Promise<ShowPromiseResult> = AdController?.show();
-        //  showPromise.then((result: ShowPromiseResult) => {
-        //     // user watch ad till the end
-        //     // your code to reward user
-        //   }).catch((result: ShowPromiseResult) => {
-        //     // user get error during playing ad or skip ad
-        //     // do nothing or whatever you want
-        //   })
-         AdController?.show().then((result: ShowPromiseResult) => {
-            // user watch ad till the end
-            // your code to reward user
-          }).catch((result: ShowPromiseResult) => {
-            // user get error during playing ad or skip ad
-            // do nothing or whatever you want
-          })
-
-     },[])
-
-    return (
-        <div className=" flex justify-center  overflow-auto">
-        <div className="w-full h-screen bg-white flex-col ">
-        <div className="flex-1 mt-5 text-start font-bold text-wrap">
-              <p className="mr-20 ml-5 text-[#ffae19]/[0.9] font-Large text-xl glow">Leaderboard</p>
-              </div>
-
-              <div className="relative overflow-x-auto mt-5 mr-4 ml-4  ">
-    <table className="w-full text-sm   text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-white uppercase  bg-[#ffae19]/[0.9] border-white border-4  border-double ">
-            <tr>
-                <th scope="col" className="text-start px-6 py-3">
-                Nº
-                </th>
-                <th scope="col" className="">
-                    Name
-                </th>
-                
-                <th scope="col" className="px-6 py-3">
-                <div className="flex grow">
+    const onReward = useCallback(() => {
+        alert('Reward');
+      }, []);
+      const onError = useCallback((result: ShowPromiseResult) => {
+        alert(JSON.stringify(result, null, 4));
+      }, []);
     
-    <div className="flex-1 text-center ">
-    <div className="flex items-center justify-center">
+      /**
+       * insert your-block-id
+       */
+      const showAd = useAdsgram({ blockId: "int-8537", onReward, onError });
     
-    <p className="">WalkCoin</p>
-    </div>
-    </div>
-    </div>
-                </th>
-            </tr>
-        </thead>
-        
-    </table>
-</div>
-
-    <div className="h-20 mt-5" />
-           </div>
-           </div>
-    )
+      return (
+        <button onClick={showAd}>Show Ad</button>
+      )
 
 }
 
---
+++ components/useAdsgram.tsx
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useRef } from 'react';
+/**
+ * Check Typescript section
+ * and use your path to adsgram types
+ */
+import type { AdController, ShowPromiseResult } from '@/types/adsgram';
+
+export interface useAdsgramParams {
+  blockId: string;
+  onReward?: () => void;
+  onError?: (result: ShowPromiseResult) => void;
+}
+
+export function useAdsgram({ blockId, onReward, onError }: useAdsgramParams): () => Promise<void> {
+  const AdControllerRef = useRef<AdController | undefined>(undefined);
+
+  useEffect(() => {
+    AdControllerRef.current = window.Adsgram?.init({ blockId, debug: true, debugBannerType: 'FullscreenMedia' });
+  }, [blockId]);
+
+  return useCallback(async () => {
+    if (AdControllerRef.current) {
+      AdControllerRef.current
+        .show()
+        .then(() => {
+          // user watch ad till the end or close it in interstitial format
+          onReward?.();
+        })
+        .catch((result: ShowPromiseResult) => {
+          // user get error during playing ad
+          onError?.(result);
+        });
+    } else {
+      onError?.({
+        error: true,
+        done: false,
+        state: 'load',
+        description: 'Adsgram script not loaded',
+      });
+    }
+  }, [onError, onReward]);
+}
