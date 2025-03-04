71838ef8ec40994c3856c8386b17f37e72b48b5e
ziqo-maker <quizziqo@gmail.com>
2025-03-04 05:41:26 -0800

    Update carosal.tsx

--- app/carosal.tsx
+++ app/carosal.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
-import { RxDotFilled } from 'react-icons/rx';
+import Image, {StaticImageData} from "next/image"
+import { GoDotFill } from "react-icons/go";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
+import test from '@/imgs/test.png';
+import Marquee from 'react-fast-marquee'
 
 function App() {
   const slides = [
     {
       url: 'https://fastly.picsum.photos/id/907/200/300.jpg?hmac=BYvJHklGn1KzEhHiZTkbQtFiRXUET5zYdLLKS6RXF3I',
+      nmb: 0
     },
     {
       url: 'https://fastly.picsum.photos/id/633/200/200.jpg?hmac=3ZyIOtFWRly1tYi_sTXjhSKzDlB-94qs6KCeIdeiCJo',
+      nmb:1
     },
   ];
 
@@ -33,38 +39,70 @@ function App() {
 
   return (
 
-    <div className=" flex-1 overflow-hidden max-w-xl mx-auto h-screen">
+    <div className=" flex-1 overflow-hidden max-w-xl mx-auto  h-screen bg-white">
       
-    <div className="bg-black w-full h-full m-auto py-16 px-4 relative group">
+    <div className="flex flex-col justify-between bg-black w-full h-full m-auto  relative group bg-white">
 
-      <div
-        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-      >
-        
-      </div>
-      {/* Left Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactLeft onClick={prevSlide} size={30} />
-      </div>
-      {/* Right Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactRight onClick={nextSlide} size={30} />
-      </div>
-      <div className='flex top-4 justify-center py-2'>
-        {slides.map((slide, slideIndex) => (
+    <div className='flex justify-between items-center mr-3 ml-3 mt-4 '>
+    <div className={`${currentIndex == 0 ? 'invisible' : ''}  text-2xl text-white`}>
+    <IoIosArrowDropleftCircle onClick={prevSlide} size={40} color='#ffb835' />
+    </div>
+      <div className='flex'>
+      {slides.map((slide, slideIndex) => (
           <div
             key={slideIndex}
             onClick={() => goToSlide(slideIndex)}
             className='text-2xl cursor-pointer'
           > 
-             
-            <RxDotFilled />
+            
+            <GoDotFill size={20} color={`${slideIndex == currentIndex ? '#ffb835' : ''}`} />
+
           </div>
+          
         ))}
       </div>
-    
+      
+      <div className={`${currentIndex == 0 ? '' : 'invisible'}  text-2xl text-white`}>
+      <IoIosArrowDroprightCircle onClick={nextSlide} size={40} color='#ffb835' />
+      </div>
 
+      </div>
+      
+      <div className="flex flex-col text-center font-bold text-wrap">
+              <p className="mr-3 ml-3  text-[#ffae19]/[0.9] font-Large text-xl glow">Unlocking Characters!</p>
+              <p className="mr-3 ml-3 mt-2 text-[#ffae19]/[0.9] font-normal  text-lg">By unlocking characters, you can increase the price of your WalkCoin tokens and walk with your favorite character.</p>
+              </div>
+               
+               <div className='flex flex-col justify-center items-center'>
+               <section className="flex  justify-center items-center w-full">
+          <div className="items-center w-full justify ">
+            <Marquee gradientWidth={120} gradient={true} className=" items-center py-2 overflow-hidden ">
+            <Image 
+                       src={test as StaticImageData}
+                        
+                     className="size-50  aspect-square rounded-xl object-cover"
+                     alt=""
+                   />
+          
+            </Marquee>
+          </div>
+        </section>
+                        
+                                 
+                       </div>
+
+          
+      
+      <div className='flex w-full justify-center items-center'>
+      <button onClick={() => {}}>
+    <div className="flex items-center w-80 rounded-full px-4 py-[12px] border-white border-4 border-double bg-[#ffae19]/[0.9] items-center justify-center">
+    <p className={`font-bold text-lg text-white glow text-nowrap`}>Next</p>
+    </div>
+     </button>  
+      </div>
+     
+     <div className='h-2 '/>
+      
     </div>
     </div>
    
