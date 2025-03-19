3488979d385ed47dc18edf14b0d5a3e2368ce8d3
ziqo-maker <quizziqo@gmail.com>
2025-03-19 01:45:31 -0700

    Update UserContextB.tsx

--- contexts/UserContextB.tsx
+++ contexts/UserContextB.tsx
@@ -26,7 +26,7 @@ export const NewUserContext = React.createContext<ContextProps>({
   });
 
 export function UserNew({ children }: { children: React.ReactNode }) {
-    const [UserDt, setUserData] = React.useState<UserDt | null>({idd:'141',gtpoint:'0',speedlvl:'1',upgrade:'1',selectcharacter:'1',value:'0.00000001',username:''});
+    const [UserDt, setUserData] = React.useState<UserDt | null>({idd:'6435568801',gtpoint:'0',speedlvl:'1',upgrade:'1',selectcharacter:'1',value:'0.00000001',username:''});
 
   const loadUserData = async () => { 
      };
