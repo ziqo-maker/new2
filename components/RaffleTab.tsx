097a14d8a72f85a3d676851a272a424683471580
ziqo-maker <quizziqo@gmail.com>
2025-04-01 02:45:33 -0700

    Update RaffleTab.tsx

--- components/RaffleTab.tsx
+++ components/RaffleTab.tsx
@@ -43,7 +43,7 @@ const defaultTx: SendTransactionRequest = {
   messages: [
     {
       // The receiver's address.
-      address: "EQCKWpx7cNMpvmcN5ObM5lLUZHZRFKqYA4xmw9jOry0ZsF9M",
+      address: "0QA0VRQbT9KhtYIdLHi8pxNT0MaNVBef3U347s1vnrlaW_O1",
       // Amount to send in nanoTON. For example, 0.005 TON is 5000000 nanoTON.
       amount: "5000000",
       // (optional) State initialization in boc base64 format.
