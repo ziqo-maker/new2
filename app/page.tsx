ade680e38e54df9e3de778dfac0b8a3e957c762c
ziqo-maker <quizziqo@gmail.com>
2025-04-05 12:32:49 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
   'use client'
-
+   
   import { useEffect, useState } from 'react'
   import { WebApp } from '@twa-dev/types'
   import { TabProvider } from '@/contexts/TabContext'
@@ -11,10 +11,44 @@
   import getGoogleplay from '@/imgs/getgoogleplay.webp';  
   import WalkTask from '@/imgs/walktask.webp';  
   import React,{useRef} from 'react';
-  import Typewriter from 'typewriter-effect';
-  import { Carousel } from "@material-tailwind/react";
-  import App from './carosal';
+    import Typewriter from 'typewriter-effect';
 import { TonConnectUIProvider } from "@tonconnect/ui-react"
+
+import { GoDotFill } from "react-icons/go";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
+import test from '@/imgs/test.png';
+import Marquee from 'react-fast-marquee'
+import alien from '@/welcomeimg/alien.png';
+import eve from '@/welcomeimg/eve.png';
+import jacky from '@/welcomeimg/jacky.png';
+import mousey from '@/welcomeimg/mousey.png';
+import swatguy from '@/welcomeimg/swatguy.png';
+import a65 from '@/gif/6-6.gif';
+import a54 from '@/gif/5-5.gif';
+import a43 from '@/gif/4-4.gif';
+import a32 from '@/gif/3-3.gif';
+import a21 from '@/gif/2-2.gif';
+
+const list: { mg: StaticImageData }[] = [
+  { mg: alien },
+  { mg: eve },
+  { mg: jacky },
+  { mg: mousey },
+  { mg: swatguy },
+
+]
+
+const listB: { mg: StaticImageData }[] = [
+  { mg: a65 },
+  { mg: a54 },
+  { mg: a43 },
+  { mg: a32 },
+  { mg: a21 },
+
+]
+
+
 
   declare global {
     interface Window {
@@ -29,7 +63,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react"
   const timerRef = useRef<NodeJS.Timeout | null>(null);
     const [refresh, setRefresh] = useState<boolean>(false);
       const [refreshB, setRefreshB] = useState<boolean>(false);
-       const [first, setFirst] = useState<boolean>(true);
+       const [first, setFirst] = useState<boolean>(false);
 
        
     useEffect(() => {
@@ -38,9 +72,8 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react"
       
       if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
         const tg = window.Telegram.WebApp
-        
         tg.ready()
-       
+
         const initDataUnsafe = tg.initDataUnsafe || {}
        
         if (initDataUnsafe.user) {
@@ -57,9 +90,13 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react"
                 
               } else {
                  setRefresh(true)
+                const gt = String(data.first)
+                  if(gt == '1'){
+                     setFirst(true)
+                  }
                 setTimeout(() => {
                   setUser(data)
-                }, 4000);
+                }, 5500);
                 
               }
             })
@@ -86,67 +123,191 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react"
       
     }, [refreshB])
 
+    const slides = [
+        {
+          nmb: 0
+        },
+        {
+          nmb:1
+        },
+      ];
+    
+      const [currentIndex, setCurrentIndex] = useState(0);
+    
+      const prevSlide = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+      };
+    
+      const nextSlide = () => {
+        const isLastSlide = currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+      };
+    
+      const goToSlide = (slideIndex:number) => {
+        setCurrentIndex(slideIndex);
+      };
 
-//     if (!user) return (
-//       <div className="container flex flex-col  mx-auto h-screen justify-between items-center w-full bg-[#ffae19] ">
-//           <div className='flex h-0'/>
-        
-//         <div className='mr-1 ml-1 flex-col text-wrap flex space-y-1 justify-center items-center text-center'>
-//         <Image 
-//         src={StartPic as StaticImageData} 
-//       className="size-20 glowbx rounded-full border-2 border-[#fda500] animate-waving-hand aspect-square object-fit"
-//       alt=""
-//     />
-//     <Typewriter
-//   options={{
-//     strings: ['Keep mining, we are getting bigger'],
-//     autoStart: true,
-//     loop: true,
-//     wrapperClassName: 'text-white text-base italic glow text-wrap text-justify'
-//   }}
-// />     
-//         </div>
+
+    if (!user) return (
+      <div className="container flex flex-col  mx-auto h-screen justify-between items-center w-full bg-[#ffae19] ">
+          <div className='flex h-0'/>
+
+       
+
+          
+            <div className='mr-2 ml-2 flex-col text-wrap flex space-y-1 justify-center items-center text-center'>
+        <Image 
+        src={StartPic as StaticImageData} 
+      className="size-20 glowbx rounded-full border-2 border-[#fda500] animate-waving-hand aspect-square object-fit"
+      alt=""
+    />
+    <Typewriter
+  options={{
+    strings: ['Earn more, play smart and keep your WalkCoin growing'],
+    autoStart: true,
+    loop: true,
+    wrapperClassName: 'text-white text-base italic glow text-wrap text-justify'
+  }}
+/>     
+        </div>
           
            
          
-//         <div className='mr-1 ml-1 flex-col text-wrap flex justify-center items-center text-center'>
-//         <div className='flex space-x-1'>
-//         <div className='flex flex-col justify-center items-center'>
-//           <Image 
-//         src={WalkTask as StaticImageData}
+       <div className='mr-1 ml-1 flex-col text-wrap flex justify-center items-center text-center'>
+        <div className='flex space-x-1'>
+        <div className='flex flex-col justify-center items-center'>
+          <Image 
+        src={WalkTask as StaticImageData}
          
-//       className="size-10  aspect-square rounded-xl object-cover"
-//       alt=""
-//     />
+      className="size-10  aspect-square rounded-xl object-cover"
+      alt=""
+    />
       
-//                   <p className="text-white font-Large">WalkTask</p>
+                  <p className="text-white font-Large">WalkTask</p>
 
-//         </div>
-//         <button onClick={() => window.open("https://play.google.com/store/apps/details?id=com.walktask.app&pcampaignid=web_share")}>
+        </div>
+        <button onClick={() => window.open("https://play.google.com/store/apps/details?id=com.walktask.app&pcampaignid=web_share")}>
           
-//           <Image 
-//            src={getGoogleplay as StaticImageData} 
-//          className="size-28 aspect-square object-cover"
-//          alt=""
-//        />
-//        </button>
+          <Image 
+           src={getGoogleplay as StaticImageData} 
+         className="size-28 aspect-square object-cover"
+         alt=""
+       />
+       </button>
        
        
       
-//        </div>
-//        <p className="text-white font-Large font-bold text-wrap">Listing Date is set for May 29, 2025</p>
-//        <div className='h-5' />
-//           </div>
+       </div>
+         <p className="text-white font-Large font-bold text-wrap">Listing Date is set for July 28, 2025</p>
+       <div className='h-5' />
+          </div>
+      </div>
+    )
+
+    if (first) return (
     
-//       </div>
-//     )
-    
-// if (first) return (
-//     <App />
-//      )
+  <div className=" flex-1 overflow-hidden max-w-xl mx-auto  h-screen bg-white">
+      
+    <div className="flex flex-col justify-between bg-black w-full h-full m-auto  relative group bg-white">
+
+    <div className='flex justify-between items-center mr-3 ml-3 mt-4 '>
+    <div className={`${currentIndex == 0 ? 'invisible' : ''}  text-2xl text-white`}>
+    <IoIosArrowDropleftCircle onClick={prevSlide} size={40} color='#ffb835' />
+    </div>
+      <div className='flex'>
+      {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className='text-2xl cursor-pointer'
+          > 
+            
+            <GoDotFill size={20} color={`${slideIndex == currentIndex ? '#ffb835' : ''}`} />
+
+          </div>
+          
+        ))}
+      </div>
+      
+      <div className={`${currentIndex == 0 ? '' : 'invisible'}  text-2xl text-white`}>
+      <IoIosArrowDroprightCircle onClick={nextSlide} size={40} color='#ffb835' />
+      </div>
+
+      </div>
+      
+      <div className="flex flex-col text-center font-bold text-wrap">
+              <p className="mr-3 ml-3 mt-1 text-[#ffae19]/[0.9] font-Large text-xl glow">{currentIndex == 0 ? 'Unlocking Characters!': 'Speed up!'}</p>
+              <p className="mr-3 ml-3 mt-1 text-[#ffae19]/[0.9] font-normal  text-lg">{currentIndex == 0 ? 'By unlocking characters, you can increase the price of your WalkCoin tokens and walk with your favorite character.': "You can earn more WalkCoin tokens by increasing your character's walking speed."}</p>
+              </div>
+               <div className={`${currentIndex ==0? 'h-9' : 'h-4'}`} />
+               <div className='flex flex-col grow justify-center items-center'>
+               <section className="flex  justify-center w-full h-full">
+          <div className="items-center justify-center items-center  ">
+            <Marquee gradientWidth={120} gradient={true} className=" items-center  overflow-hidden ">
+            
+            {
+            list.map((mg,index) => {
+              return (
+                <Image 
+                 key={index}
+                src={mg.mg}
+              className={`${currentIndex == 0 ? 'w-full h-full' : 'w-0 h-0'}  aspect-square  object-cover`}
+              alt=""
+            />
+              )
+            })
+            }
+
+            {
+              listB.map((mg,index) => {
+                return (
+                  <div key={index} className={`${currentIndex == 0 ? 'w-0 h-0 p-0 p-0' : 'flex  h-80 p-5'} `}> 
+            <div className={`${currentIndex ==0? 'w-0 h-0' : 'flex grow w-full h-full relative  rounded-full border-4 border-double'}`}>
+           
+           <Image 
+                  src={mg.mg}
+                  className={`${currentIndex == 0 ? 'w-0 h-0' : 'w-full h-full'}  aspect-square rounded-full object-fill`}
+                  alt=""
+              />
+        
+            </div> 
+   
+          </div>
+                
+                )
+              })
+            }
+           
+          
+            </Marquee>
+          </div>
+        </section>
+                        
+                                 
+                       </div>
+
+          
+      
+      <div className='flex w-full justify-center items-center'>
+      <button onClick={currentIndex == 0 ? () => {goToSlide(1)} : () => {setFirst(false)}}>
+    <div className="flex items-center w-80 rounded-full px-4 py-[12px] border-white border-4 border-double bg-[#ffae19]/[0.9] items-center justify-center">
+    <p className={`font-bold text-lg text-white glow text-nowrap`}>{currentIndex == 0 ? 'Next': 'Start'}</p>
+    </div>
+     </button>  
+      </div>
+     
+     <div className='h-5 '/>
+      
+    </div>
+    </div>
+  
+)
 
     return (
-            <UserNew>
+         <UserNew>
           <TonConnectUIProvider manifestUrl="https://emerald-rear-parrotfish-934.mypinata.cloud/ipfs/bafkreiefycb6u266ssmwexxziehzmqoffbfaumzp3dbsa2vdak6sujnffm">
     <TabProvider>
           <main className='main-h-screen content-around bg-white '>
@@ -156,7 +317,6 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react"
         </TabProvider>
         </TonConnectUIProvider>
         </UserNew>
-   
       
     )
   }
