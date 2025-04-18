ea961d2720874c2bc39d691b513e60fc496d6778
ziqo-maker <quizziqo@gmail.com>
2025-04-18 11:23:16 -0700

    Add files via upload

---
+++ app/api/update-ticket/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { prisma } from '@/lib/prisma'
+
+export async function POST(req: NextRequest) {
+    try {
+        
+        const {idd,ticket} = await req.json()
+
+        await prisma.ticket.updateMany({
+            where: { idd },
+            data: { 
+                ticket
+            }
+        })
+        
+        return NextResponse.json({success :true})
+    } catch (error) {
+        console.error('Error processing user data:', error)
+        return NextResponse.json({ error: 'server error' }, { status: 500 })
+    }
+}
