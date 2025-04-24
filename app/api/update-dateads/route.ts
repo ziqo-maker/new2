02313d9b287f0732fca6609531d7fedf4367bfe5
ziqo-maker <quizziqo@gmail.com>
2025-04-24 13:29:21 -0700

    Update route.ts

--- app/api/update-dateads/route.ts
+++ app/api/update-dateads/route.ts
@@ -7,15 +7,17 @@ export async function POST(req: NextRequest) {
         const {idd} = await req.json()
             const newdate = new Date()
             const increase = newdate.getTime() +(24*60*60*1000)
-         await prisma.watchads.updateMany({
+       await prisma.watchads.updateMany({
             where: { idd },
             data: { 
                 dateMining:new Date(increase),
                 cnt:0
              }
         })   
-        
-        return NextResponse.json({success :true})
+        let userB = await prisma.watchads.findFirst({
+            where: { idd }
+        })
+        return NextResponse.json({success:true,cnt:String(userB?.cnt),dt:String(userB?.date),dtMining:String(userB?.dateMining)})
     } catch (error) {
         console.error('Error processing user data:', error)
         return NextResponse.json({ error: 'server error' }, { status: 500 })
