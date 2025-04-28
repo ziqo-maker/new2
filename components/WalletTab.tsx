3b7fd821860dbcba74f8bab3bb543bcf19bcc095
ziqo-maker <quizziqo@gmail.com>
2025-04-28 12:19:27 -0700

    Update WalletTab.tsx

--- components/WalletTab.tsx
+++ components/WalletTab.tsx
@@ -11,6 +11,15 @@ import { NewUserContext } from '@/contexts/UserContextB';
 import Multiply from '@/icons/multiply.svg';
 import Support from '@/icons/support.svg';
 import TetherB from '@/icons/tetherb.svg';
+import Equal from '@/icons/equal.svg';
+import alien from '@/welcomeimg/alien.png';
+import eve from '@/welcomeimg/eve.png';
+import jacky from '@/welcomeimg/jacky.png';
+import mousey from '@/welcomeimg/mousey.png';
+import swatguy from '@/welcomeimg/swatguy.png';
+import toy from '@/welcomeimg/toy.png';
+import unlock from '@/icons/unlock.svg';
+import lock from '@/icons/lockb.svg';
 
 
 type Task = {
@@ -28,102 +37,110 @@ const WalletTab = () => {
     const [Loading,setLoading] = useState<boolean> (true);
        const [refresh, setRefresh] = useState<boolean>(false);
 
-          useEffect(() => {
-            setTimeout(() => {
-              setRefresh(true)
-            }, 1000);  
-            try {
-              fetch('/api/get-withdraw', {
-               method: 'POST',
-               headers: {
-                 'Content-Type': 'application/json',
-               },
-               body: JSON.stringify({ idd: String(UserDt?.idd) }),
-             })
-             .then((res) => res.json())
-             .then((data) => {
-              if(data.success){
+          // useEffect(() => {
+          //   setTimeout(() => {
+          //     setRefresh(true)
+          //   }, 1000);  
+          //   try {
+          //     fetch('/api/get-withdraw', {
+          //      method: 'POST',
+          //      headers: {
+          //        'Content-Type': 'application/json',
+          //      },
+          //      body: JSON.stringify({ idd: String(UserDt?.idd) }),
+          //    })
+          //    .then((res) => res.json())
+          //    .then((data) => {
+          //     if(data.success){
                
-                var nmb = 1
-                data.all.forEach((t: any)=> {
+          //       var nmb = 1
+          //       data.all.forEach((t: any)=> {
                   
-                  let model = {
-                    id:String(nmb),
-                    date:t.date,
-                    amount:t.amount,
-                    status:t.status
-                 }
-                 nmb++
-                 gtTasks.push(model)
-                 setLoading(false)
-                })
+          //         let model = {
+          //           id:String(nmb),
+          //           date:t.date,
+          //           amount:t.amount,
+          //           status:t.status
+          //        }
+          //        nmb++
+          //        gtTasks.push(model)
+          //        setLoading(false)
+          //       })
               
-              }
-             })
-           } catch (err) {
+          //     }
+          //    })
+          //  } catch (err) {
             
-           }
+          //  }
            
-          },[refresh])
+          // },[refresh])
     
     return (
         <div className=" flex justify-center  overflow-auto">
          <div className="w-full h-screen bg-white flex-col ">
 
          <div className="flex-col w-full mt-4   items-center justify-start items-center">
-             <div className="flex grow ml-2 mr-2">
-            <div className="flex  bg-[#ffae19]/[0.9]  items-center   rounded-full px-1 py-[3px] ">
-        <Image 
-        src={FootPrint as StaticImageData} 
-      className="w-8 h-8 aspect-square object-cover"
-      alt="Shiba Inu"
-    />
-              <div className="flex flex-col text-center mr-2 ml-1 ">
-              <div className="flex items-center justify-center ">
-              <p className=" text-white font-normal  text-base ">Balance</p>
+            <div className="flex w-full items-center justify-center ">
+
+            <div className="flex  items-center  rounded-full ">
+              <div className="flex flex-col text-center bg-[#ffae19]/[0.9] text-white border-4 border-white rounded-xl border-double  ">
+              <div className="flex-col ro items-center justify-center px-2 mt-1 ">
+              <p className="text-white   font-bold  text-sm ">WalkCoin Balance</p>
               </div>
-              <div className="flex items-center justify-center">
-              <p className=" text-white font-bold glow text-base text-wrap">{Number(UserDt?.gtpoint).toLocaleString()}</p>
+              <div className="flex items-center rounded-lg bg-white mt-1 px-1 py-1 justify-center">
+              <p className=" text-black font-bold glow text-[13px] text-wrap">{Number(UserDt?.gtpoint).toLocaleString()}</p>
               </div>
               </div>
               </div>
               <div className="flex items-center justify-center px-2">
               <Image 
         src={Multiply as StaticImageData} 
-      className="w-5 h-6 aspect-square object-cover"
-      alt="Shiba Inu"
+      className="w-5 h-5 aspect-square object-cover"
+      alt=""
     />
               </div>
-              
-              <div className="flex  bg-[#ffae19]/[0.9]  items-center   rounded-full px-1 py-[3px] ">
-        <Image 
-        src={DollarB as StaticImageData} 
-      className="w-8 h-8 aspect-square object-cover"
-      alt="Shiba Inu"
-    />
-              <div className="flex flex-col text-center mr-2 ">
-              <div className="flex items-center justify-center ">
-              <p className=" text-white font-normal  text-base ">WalkCoin Price</p>
+              <div className="flex  items-center  rounded-full ">
+              <div className="flex flex-col text-center bg-[#ffae19]/[0.9] text-white border-4 border-white rounded-xl border-double  ">
+              <div className="flex-col ro items-center justify-center px-2 mt-1 ">
+              <p className="text-white   font-bold  text-sm ">WalkCoin Price</p>
               </div>
-              <div className="flex items-center justify-center ">
-              <p className=" text-white font-bold glow text-base text-wrap">{(Number(UserDt?.value)).toFixed(8)}</p>
+              <div className="flex items-center rounded-lg bg-white mt-1 px-1 py-1 justify-center">
+              <p className=" text-black font-bold glow text-[13px] text-wrap">{(Number(UserDt?.value)).toFixed(8)}</p>
               </div>
               </div>
-              </div>
+              </div>            
+          
             </div>
 
-            <div className="flex grow  mt-1 mr-1 ">
+            <div className="flex w-full justify-center items-center  mt-2">
             <div className="flex items-center justify-start px-2 space-x-1">
-              <p className=" text-black font-semibold font-serif glow text-sm text-wrap">= {(Number(UserDt?.gtpoint)*Number(UserDt?.value)).toFixed(8)}</p>
-              <Image 
-        src={Tether as StaticImageData} 
+            <Image 
+        src={Equal as StaticImageData} 
       className="w-6 h-6 aspect-square object-cover"
       alt="Shiba Inu"
     />
+    <div className="flex  items-center   rounded-full ">
+              <div className="flex flex-col text-center bg-[#ffae19]/[0.9] text-white border-4 border-white rounded-xl border-double  ">
+              <div className="flex-col ro items-center justify-center px-2 mt-1 ">
+              <p className="text-white   font-bold  text-sm ">WalkCoin Burning Reward</p>
               </div>
+              <div className="flex items-center rounded-lg space-x-1 bg-white mt-1 px-1 py-1 justify-center">
+              <p className=" text-black font-bold glow text-[13px] text-wrap">{(Number(UserDt?.gtpoint)*Number(UserDt?.value)).toFixed(8)}</p>
+              <Image 
+        src={Tether as StaticImageData} 
+      className="w-5 h-5 aspect-square object-cover"
+      alt=""
+    />
+              </div>
+              </div>
+              </div>
+          
+              </div>
+              <div className="w-4" />
             </div>
            
-           <div className="flex mt-5 ml-4 mr-4">
+           <div className="flex flex-col mt-3 ml-4 mr-4 space-y-1">
+
            <div className="flex w-full  items-center justify-center items-center space-x-2">
                 
      
@@ -134,26 +151,192 @@ const WalletTab = () => {
                        <div className="flex items-center space-x-1 justify-center">
                        <Image 
                  src={TetherB as StaticImageData} 
-               className="w-7 h-7 aspect-square object-cover"
-               alt="Shiba Inu"
+               className="w-9 h-7 aspect-square object-cover"
+               alt=""
              />
-                  <p className=" text-white font-semibold text-lg truncate">request a withdrawal</p>
+                  <p className=" text-white font-bold glow text-[16px] truncate">Burn WalkCoin</p>
                        </div>
                        </div>
                        </button>
               
-                       <button onClick={() => {window.open("https://t.me/WalkCoinSupportbot")}} className="flex bg-[#ffae19]/[0.9] items-center text-wrap  rounded-full px-3   py-[10px] ">
+                       <button onClick={() => {window.open("https://t.me/WalkCoinSupportbot")}} className="flex border-white border-double border-4 bg-[#ffae19]/[0.9] items-center text-wrap  rounded-full p-3 justify-center ">
+                 <Image 
+                 src={Support as StaticImageData} 
+               className="w-5 h-5 aspect-square object-cover"
+               alt=""
+             />
+                     
+                       </button>
+                       
+                </div>
+                <p className="text-black/[0.8] ml-1 text-sm text-wrap font-bold text-start">Complete Task below to be aligble for airdrop</p>
+
+                <div className="w-full py-4">
+  <div className="relative flex items-center justify-between w-full">
+    <div className="absolute left-0 top-2/4 h-0 w-full -translate-y-2/4 bg-[#ffae19]/[0.7]">
+    </div>
+    <div className="absolute left-0 top-2/4 h-0.5 w-full -translate-y-2/4  bg-[#ffae19]/[0.7] transition-all duration-500">
+    </div>
+    
+    <div
+      className="relative z-10 grid w-20 h-20 font-bold text-white transition-all duration-300 bg-white rounded-full place-items-center">
+         <Image 
+        src={toy as StaticImageData} 
+      className="w-20 h-20 aspect-square object-cover"
+      alt=""
+    />
+      <div className="absolute -bottom-[1.5rem] w-max text-center">
+        <h6
+          className="block font-sans text-sm antialiased font-bold leading-relaxed tracking-normal text-[#ffae19]/[0.9]">
+          Ty
+        </h6>
+      
+      </div>
+      <div className="w-full mt-7 items-center justify-center">
+          <center>
+          <Image 
+        src={unlock as StaticImageData} 
+      className="w-10 h-10 aspect-square   object-cover"
+      alt=""
+    />
+          </center>
+       
+        </div>
+    </div>
+    <div
+      className="relative z-10 grid w-20 h-20 font-bold text-white transition-all duration-300 bg-white rounded-full place-items-center">
+         <Image 
+        src={mousey as StaticImageData} 
+      className="w-20 h-20 aspect-square object-cover"
+      alt=""
+    />
+      <div className="absolute -bottom-[1.5rem] w-max items-center justify-center ">
+        <h6
+          className="block font-sans text-sm antialiased font-bold leading-relaxed tracking-normal text-[#ffae19]/[0.9]">
+          Mousey
+        </h6>
+       
+        
+      </div>
+      <div className="w-full mt-8  items-center justify-center">
+          <center>
+          <Image 
+        src={lock as StaticImageData} 
+      className="w-8 h-8 aspect-square   object-cover"
+      alt=""
+    />
+          </center>
+       
+        </div>
+    </div>
+    <div
+      className="relative z-10 grid w-20 h-20 font-bold text-white transition-all duration-300 bg-white rounded-full place-items-center">
+        <Image 
+        src={jacky as StaticImageData} 
+      className="w-20 h-20 aspect-square object-cover"
+      alt=""
+    />
+      <div className="absolute -bottom-[3rem] w-max text-center items-center justify-center">
+        <h6
+          className="block font-sans text-sm antialiased font-bold leading-relaxed tracking-normal text-[#ffae19]/[0.9]">
+          Jackie
+        </h6>
+        <p className="block font-sans text-base antialiased font-normal leading-relaxed text-black">
+        
+        </p>
+      </div>
+    </div>
+    <div
+      className="relative z-10 grid w-20 h-20 font-bold text-gray-900 transition-all duration-300 bg-white rounded-full place-items-center">
+        <Image 
+        src={swatguy as StaticImageData} 
+      className="w-20 h-20 aspect-square object-cover"
+      alt=""
+    />
+      <div className="absolute -bottom-[3rem] w-max text-center items-center justify-center">
+        <h6
+          className="block font-sans text-sm antialiased font-bold leading-relaxed tracking-normal text-[#ffae19]/[0.9]">
+          Swat Guy
+        </h6>
+        <p className="block font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
+         
+        </p>
+      </div>
+    </div>
+    <div
+      className="relative z-10 grid w-20 h-20 font-bold text-gray-900 transition-all duration-300 bg-white rounded-full place-items-center">
+        <Image 
+        src={eve as StaticImageData} 
+      className="w-20 h-20 aspect-square object-cover"
+      alt=""
+    />
+      <div className="absolute -bottom-[3rem] w-max text-center items-center justify-center">
+        <h6
+          className="block font-sans text-sm antialiased font-bold leading-relaxed tracking-normal text-[#ffae19]/[0.9]">
+          Eve
+        </h6>
+        <p className="block font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
+          
+        </p>
+      </div>
+    </div>
+  
+    <div
+      className="relative z-10 grid w-20 h-20 font-bold text-gray-900 transition-all duration-300 bg-white rounded-full place-items-center">
+        <Image 
+        src={alien as StaticImageData} 
+      className="w-20 h-20 aspect-square object-cover"
+      alt=""
+    />
+      <div className="absolute -bottom-[3rem] w-max text-center items-center justify-center">
+        <h6
+          className="block font-sans text-sm antialiased font-bold leading-relaxed tracking-normal text-[#ffae19]/[0.9]">
+          Alien Soldier
+        </h6>
+        <p className="block font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
+         
+        </p>
+      </div>
+    </div>
+  </div>
+  <div className="flex justify-between mt-20">
+   
+  </div>
+</div>
+
+           <div className="flex w-full  items-center justify-center text-center space-x-2">
+                
+     
+                <button onClick={() => {}} className="flex flex-grow opacity-60 bg-[#ffae19]/[0.9] border-white border-4  border-double items-center justify-center text-center text-wrap  rounded-2xl px-1 py-[8px] ">
+                 
+
+                       <div className="flex-1 text-center">
+                       <div className="flex items-center space-x-1 justify-center">
+                       <Image 
+                 src={FootPrint as StaticImageData} 
+               className="w-7 h-7 aspect-square object-cover"
+               alt="Shiba Inu"
+             />
+                  <p className=" text-white font-semibold text-lg truncate">Airdrop</p>
+                       </div>
+                       </div>
+                       </button>
+              
+                       <div className="flex invisible bg-[#ffae19]/[0.9] items-center text-wrap   rounded-full px-3   py-[10px] ">
                  <Image 
                  src={Support as StaticImageData} 
                className="w-8 h-8 aspect-square object-cover"
                alt="Shiba Inu"
              />
                      
-                       </button>
+                       </div>
                        
                 </div>
+
+               <p className="text-black ml-1 text-base text-wrap font-bold text-start">Listing Date is set for May 29, 2025</p>
+               <p className="text-black ml-1 text-sm text-wrap">Note: We decided to appreciate user activity and help the project grow. From now on, you can exchange your WalkCoin to USDT before listing.</p>
+                
            </div>
-           
            
            <div className='flex flex-col mt-2  justify-start items-start  mr-4 ml-4'>
       <p className={`font-bold text-base  text-black text-wrap`}>Details</p> 
