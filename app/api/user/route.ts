bdee6cb04d32f8496fa9c95ed8a7150f14f29535
ziqo-maker <quizziqo@gmail.com>
2025-05-02 14:49:43 -0700

    Update route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import React,{useState} from 'react';
   
-  
+   
 export async function POST(req: NextRequest) {
     try {
         const userData = await req.json()
