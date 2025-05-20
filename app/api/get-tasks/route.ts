fe15b7e83f30779372b90bc22304a8dc1c270b96
ziqo-maker <quizziqo@gmail.com>
2025-05-20 07:27:19 -0700

    Update route.ts

--- app/api/get-tasks/route.ts
+++ app/api/get-tasks/route.ts
@@ -10,7 +10,6 @@ export async function POST(req: NextRequest) {
         const str: string = String(user?.donetasks);
         const t = str.split(',').map(Number);
         const getTasks = await prisma.doing.findMany({
-          take: 7,
             where: {
               NOT: {
                 id: {in: t}
